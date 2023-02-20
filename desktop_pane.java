497f4d994f352099dd056a50f12f7a0a10572972
AngelAld <aaaldana50@gmail.com>
2023-02-20 17:39:16 -0500

    Updated at: 2023-02-20 17:39:16 by Araghi

--- desktop_pane.java
+++ desktop_pane.java
@@ -1,18 +1,16 @@
 
-ImageIcon icon = new ImageIcon(getClass().getResource("/recursos/back.jpg"));
-Image image = icon.getImage();
+//import java.awt.Graphics;
+//import java.awt.Image;
+//import javax.swing.ImageIcon;
 
-jDesktopPane1 = new javax.swing.JDesktopPane(){
- public void paintComponent(Graphics g){
- g.drawImage(image,0,0,getWidth(),getHeight(),this);
- }
-};
+//ImageIcon icon = new ImageIcon(getClass().getResource("/recursos/back.jpg"));
+//Image image = icon.getImage();
 
-
-
-import java.awt.Graphics;
-import java.awt.Image;
-import javax.swing.ImageIcon;
+//jDesktopPane1 = new javax.swing.JDesktopPane(){
+// public void paintComponent(Graphics g){
+// g.drawImage(image,0,0,getWidth(),getHeight(),this);
+// }
+//};
 
 
 
@@ -21,16 +19,6 @@ import javax.swing.ImageIcon;
 
 
 
-ImageIcon icon = new ImageIcon(imagePath);
-            Image image = icon.getImage();
-
-            
-
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(newimage, 0, 0, this);
-            }
 
 
 
